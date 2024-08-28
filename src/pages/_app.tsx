b62bc26fd4dea822ed0a573d125222c49f4ec0d6
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-29 01:21:44 +0900

    webvitals
    useReportWebVitals를 이용한 성능분석

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider, Hydrate } from 'react-query'
 import { SessionProvider } from 'next-auth/react'
+import { useReportWebVitals } from 'next/web-vitals'
 import { AlertContextProvider } from '@contexts/AlertContext'
 import { Global } from '@emotion/react'
 import ErrorBoundary from '@shared/ErrorBoundary'
@@ -15,6 +16,10 @@ export default function App({
   Component,
   pageProps: { dehydratedState, session, ...pageProps },
 }: AppProps) {
+  useReportWebVitals((metric) => {
+    console.log(metric)
+  })
+
   return (
     <Layout>
       <Global styles={globalStyles} />
